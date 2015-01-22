b34b6d8c3f2b9deb309b7460534602b7f10b006d
chunliang <chunliang.li@urovo.com>
2015-01-22 10:56:59 +0800

    at update dir: sh script/sign.sh tmp/a.zip z-sign.zip

--- sign.sh
+++ sign.sh
@@ -3,4 +3,4 @@
 #
 #sign update.zip
 #
-java -Xmx2048m -jar ../../out/host/linux-x86/framework/signapk.jar -w ../../build/target/product/security/testkey.x509.pem ../../build/target/product/security/testkey.pk8 $1 $2
+java -Xmx2048m -jar ../out/host/linux-x86/framework/signapk.jar -w ../build/target/product/security/testkey.x509.pem ../build/target/product/security/testkey.pk8 $1 $2
